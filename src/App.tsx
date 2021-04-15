338fab1f9c5d2831feabbd3d54c748067dc486a4
Brittney <brittney.cavazos@gmail.com>
2021-04-15 16:35:21 -0700

    built list of pokemon for front page, added minor styling

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ const App: React.FC = () => {
       <div className="row text-center">
           {pkmnData.map(pkmn=>{
             return(
-              <div className="col">
-                <div className="card">
+              <div className="col-3 py-3">
+                <div className="card px-5 pt-3">
                 <img className="card-img-top" src={pkmn.img}alt={pkmn.name}/>
                 <div className="card-body">
                   <h5 className="card-title">{pkmn.name}</h5>
